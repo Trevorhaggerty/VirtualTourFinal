02f7efefdb2a9ec665c297999f7d15b7abcdde89
Trevorhaggerty <cesismalon@gmail.com>
2015-08-06 13:46:49 -0700

    testing

--- Assets/OVR/Scripts/OVRCameraRig.cs
+++ Assets/OVR/Scripts/OVRCameraRig.cs
@@ -15,7 +15,11 @@ Unless required by applicable law or agreed to in writing, the Oculus VR SDK
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.
+limitat
+
+
+
+
 
 ************************************************************************************/
 
